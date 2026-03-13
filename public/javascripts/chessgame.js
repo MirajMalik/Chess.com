149e2a371f03ef1cca7307219820e6356a765936
MirajMalik <mirajmalik333@gmail.com>
2026-03-13 16:37:53 +0600

    added tailwind,chess.min.js cdn

--- public/javascripts/chessgame.js
+++ public/javascripts/chessgame.js
@@ -1,7 +1,32 @@
+const { Chess } = require("chess.js");
+
 const socket = io();
+const chess = new Chess();
 
-socket.emit("Ok")                                              // emit(event)-> means sending a event from frontend to backend, in backend io.on will receive this event through uniquesocket and will execute a function 
 
-socket.on("ok ok got it", function() {                         // received ok ok got it event from backend
-    console.log("ok ok got it received");
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// socket.emit("Ok")                                              // emit(event)-> means sending a event from frontend to backend, in backend io.on will receive this event through uniquesocket and will execute a function 
+
+// socket.on("ok ok got it", function() {                         // received ok ok got it event from backend
+//     console.log("ok ok got it received");
+// })
