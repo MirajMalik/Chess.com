9973c3ca260b185c97560e758cc578e153513757
MirajMalik <mirajmalik333@gmail.com>
2026-03-13 17:50:33 +0600

    css and full board functionality added

--- public/javascripts/chessgame.js
+++ public/javascripts/chessgame.js
@@ -26,9 +26,50 @@ const renderBoard = () => {
             if(square) {
                 const pieceElement = document.createElement("div");
                 pieceElement.classList.add("piece", square.color === "w" ? "white" : "black");
+
+            pieceElement.innerHTML = "";
+            pieceElement.draggable = playerRole === square.color;
+
+            pieceElement.addEventListener("dragstart", (e) => {
+                if(pieceElement.draggable) {
+                    draggedPiece = pieceElement;
+                    sourceSquare = { row : rowIndex, col: squareIndex };
+                    e.dataTransfer.setData("text/plain", "");
+                }     
+            });
+
+
+            pieceElement.addEventListener("dragend", (e) => {
+                draggedPiece = null;
+                sourceSquare = null;
+            });
+
+            squareElement.appendChild(pieceElement);
+
             }
 
+            squareElement.addEventListener("dragover", (e) => {
+                e.preventDefault();
+            });
+
+            squareElement.addEventListener("drop", (e) => {
+                e.preventDefault();
+                if(draggedPiece) {
+                    const targetSource = {
+                        row: parseInt(squareElement.dataset.row),
+                        col: parseInt(squareElement.dataset.col),
+                    }
+
+                    handleMove(sourceSquare, targetSource);
+                }
+            });
+
+
+
+
+     
         });
+        boardElement.appendChild(squareElement);
     });
 };
 
